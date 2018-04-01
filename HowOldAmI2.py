e88442823170620acc3dc148df5805e7b56379cd
Henry Kravchenko <hk@kdev.com>
2018-03-31 22:54:13 -0400

    works if you pass date as yyy/MM/dd\

--- HowOldAmI2.py
+++ HowOldAmI2.py
@@ -1,13 +1,32 @@
 #HowOldAmI2.py
 from lxml import html
 import requests
+import sys
+import datetime
 
-page = requests.get("http://www.datetime.io/age/1989/1/1")
+#page = requests.get("http://www.datetime.io/age/1989/1/1")
+#tree = html.fromstring(page.content)
+
+print (len(sys.argv))
+
+#birthdate = datetime.datetime.strptime(sys.argv[1], %y)  #(sys.argv[1]) #+ '/' + datetime.date.month(sys.argv[1]) + '/' + datetime.date.day(sys.argv[1])
+#print (birthdate)
+#if not isinstance(birthdate, datetime(datetime.date)):
+#    print ('You must pass a valid birth date. Exiting...')
+#    exit(1)
+
+if len(sys.argv) < 2:
+    print ( 'You must pass your date of birth. Exiting....')
+    exit(1)
+
+
+page = requests.get("http://www.datetime.io/age/" + sys.argv[1])
 tree = html.fromstring(page.content)
 
 
-myage = tree.xpath('/html/body/div[1]/h2/span/text()')
-print('my age: ',  myage)
+myage = str(tree.xpath('/html/body/div[1]/h2/span/text()')).strip('[\'').strip('\']')
+dateofbith = str(tree.xpath('/html/body/div[1]/p/text()')).strip('[\'\\n\\t').strip('!\\n\']')
+print ('You are ' + myage + '. ' + dateofbith)
 
 #/html/body/div[12]/span
 #body > div.content > h2 > span
